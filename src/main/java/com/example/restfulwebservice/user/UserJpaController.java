4d55e5e95c0a828a32cb583df3c2965213236c07
mina <cnm37@naver.com>
2022-10-05 16:56:18 +0900

    [JPA Persistence API 사용] ~ 게시물 조회를 위한 POST Entity와 User Entity와의 관계 설정

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -1,68 +1,73 @@
 package com.example.restfulwebservice.user;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-public class UserController {
-    private UserDaoService service;
+@RequestMapping("/jpa")
+public class UserJpaController {
+    @Autowired
+    private UserRepository userRepository;
 
-    public UserController(UserDaoService service) {
-        this.service = service;
-    }
-
+    // http://localhost:8088/jpa/users
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
-        return service.findAll();
+        return userRepository.findAll();
     }
 
-    // GET /users/1 or /users/10 -> String
     @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
-        User user = service.findOne(id);
+        Optional<User> user = userRepository.findById(id);
 
-        if(user == null) {
-            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        if(!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID{%s} not found", id));
         }
 
-        // HATEOAS
-        EntityModel<User> resource = EntityModel.of(user);
+        EntityModel<User> resource = EntityModel.of(user.get());
         WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         resource.add(linkTo.withRel("all-users"));
 
         return resource;
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = service.save(user);
+        User savedUser = userRepository.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
+                .path("{id}")
                 .buildAndExpand(savedUser.getId())
                 .toUri();
 
         return ResponseEntity.created(location).build();
     }
 
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable int id) {
-        User user = service.deleteById(id);
+    @GetMapping("/users/{id}/posts")
+    public List<Post> retrieveAllPostsByUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
 
-        if(user == null) {
-            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        if(!user.isPresent()) {
+            throw new UserNotFoundException(String.format("ID{%s} not found", id));
         }
+
+        return user.get().getPosts();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users values(90001, now(), 'User1', 'test1111', '701010-11111111');
+insert into users values(90002, now(), 'User2', 'test2222', '801010-22222222');
+insert into users values(90003, now(), 'User3', 'test3333', '901010-33333333');
+
+insert into post values(1001, 'My first post', 90001);
+insert into post values(1002, 'My second post', 90001);
